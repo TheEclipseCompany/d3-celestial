3d9f29c676e71af9397ae7b396be07cd7a53e584
Stephen Watkins <swatkins@easypost.com>
2024-09-17 10:19:15 -0400

    fix

--- src/celestial.js
+++ src/celestial.js
@@ -1,6 +1,6 @@
 /* global module, require, topojson, settings, bvcolor, projections, projectionTween, poles, eulerAngles, euler, getAngles, transformDeg, getData, getPlanets, getPlanet, listConstellations, getConstellationList, getMwbackground, getGridValues, Canvas, halfπ, $, px, Round, has, hasCallback, isArray, isNumber, arrayfy, form, geo, fldEnable, setCenter, interpolateAngle, formats */
 var Celestial = {
-  version: '0.7.35',
+  version: '0.8.1',
   container: null,
   data: []
 };
